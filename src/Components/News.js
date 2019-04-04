ab35e8faef6822c9ef2bbb65690dcdf77d9cc82a
Greg <gregoire.suray@gmail.com>
2019-04-04 11:43:12 +0200

    startnewspage

---
+++ src/Components/News.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class News extends Component {
+    render() { 
+        return (
+        <div>
+           <div>
+               <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiuICYTNUKTm2ZMtmQ3ULgd5to4zch5vNWcY7RCE4HPCRJ7c5v2Q" alt="news"/>
+           </div>
+           <div>
+                <p className="newscontent">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+           </div>
+        </div>
+        );
+    }
+}
+ 
+export default News;
