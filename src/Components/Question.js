8eef1160c62632f77ba9ac9e5d0407c78c75c695
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-04-04 14:51:40 +0200

    modify question component

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -3,7 +3,6 @@ import { Button } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
 
 const questions = {
-    picture: "https://static.ostadium.com/galleries/world-cup-football-2014-illus.jpg",
     question: "Which team won the FIFA World Cup in 2014 ?",
     answer1: "SPAIN",
     answer2: "FRANCE",
@@ -17,12 +16,11 @@ export default class Questions extends React.Component {
         return (
             <Container>
                 <Row>
-                    <Col><h1 className="question">{questions.question}</h1></Col>
-                </Row>
-                <Row>
-                    <Col><img src={questions.picture}alt="pictureOfQuestion"/></Col>
-                </Row>
-                <Row>
+                    <div className="questionBox">
+                    <Col><h2 className="question">{questions.question}</h2></Col>
+                    </div>                       
+                </Row>         
+                <Row>   
                     <Col><Button outline color="primary">{questions.answer1}</Button></Col>
                     <Col><Button outline color="primary">{questions.answer2}</Button></Col>
                 </Row>
