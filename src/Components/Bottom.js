015ffa0a5d39c051869059a0a21830ff2d6e173c
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-04 11:41:02 +0200

    Created answers buttons

---
+++ src/Components/Bottom.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Container, Row, Col } from 'reactstrap';
+
+class ArticleChoice extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <article>
+                <Container>
+                    <Row>
+                        <Col><h1 className="mt-3">Do you want more informations about your answer ?</h1></Col>
+                    </Row>
+                    <Row>
+                        <Col className="mt-3"> <Button className="ButtonSize" color="success">Yes</Button>{' '} </Col>
+                        <Col className="mt-3">  <Button className="ButtonSize" color="danger">No</Button>{' '} </Col>
+                    </Row>
+                </Container>
+            </article>
+         );
+    }
+}
+ 
+export default ArticleChoice;
