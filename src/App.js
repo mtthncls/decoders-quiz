d9cdb4a88b4fcde22e64517999c2199263886913
Sébastien Jolly <seb_879@hotmail.fr>
2019-04-09 17:36:28 +0200

    call API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import Questions from './Components/Question'
+import Questions from './Components/Questions'
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+      .then(response  =>  response.json())
+      .then(data  => {
+        this.setState({
+          questions: data.results[0],
+        });
+    });
+}
   render() {
     return (
-      <Questions></Questions>
+      <Questions questions={this.state.questions} />
     );
   }
 }
---
+++ src/Components/Questions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+
+const Questions = ({ questions }) => {
+        return (
+            <Container>
+                <header className="appHeader">
+                    <p>Question #1 in</p>
+                    <p className="categoryHeader">{questions.category}</p>
+                    <p className="questionHeader">{questions.question}</p>
+                </header>
+                <Row>
+                    <Col><Button outline color="primary" className="test">{questions.correct_answer}</Button></Col>
+                    <Col><Button outline color="primary">{questions.incorrect_answers}</Button></Col>
+                </Row>
+                <Row>
+                    <Col><Button outline color="primary">{questions.incorrect_answers}</Button></Col>
+                    <Col><Button outline color="primary">{questions.incorrect_answers}</Button></Col>
+                </Row>
+            </Container>
+        );
+    }
+
+export default Questions;
