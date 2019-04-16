cf5701d7c002fa77a7bffad1dbdec51da134786b
Matthieu NICOLAS <mtthncls@gmail.com>
2019-04-16 15:38:01 +0200

    display grey buttons in Question for left non correct answers after the question has been answered

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      currentNewsArticle: {},
-      preferredNewsArticles: [],
-        answered: false, //has one button been clicked or not ?
-        answerStatus: false, //is the answer true or false ?
-        buttonClicked: "",   //which button has been clicked ?
-        isButtonDisabled: false,
-        question: {
-            category: "Currency",
-            question: "What was the maximum value the Bitcoin reached ?",
-            answers: this.randomizeAnswersDisplay([
-                { text: "19 500 $", correct: true },
-                { text: "176 467 $", correct: false },
-                { text: "9099 $", correct: false },
-                { text: "1290 $", correct: false }
-            ]),
-        }
+      currentNewsArticle: {}, //ArticleSetChoice component
+      preferredNewsArticles: [], //ArticleSetChoice component
+      answered: false, //Question component : has one button been clicked or not ?
+      answerStatus: false, //Question component : is the answer true or false ?
+      buttonClicked: "",   //Question component : which button has been clicked ?
+      isButtonDisabled: false, //Question component : button clickable or not
+      question: {
+        category: "Currency",
+        question: "What was the maximum value the Bitcoin reached ?",
+        answers: this.randomizeAnswersDisplay([
+          { text: "19 500 $", correct: true },
+          { text: "176 467 $", correct: false },
+          { text: "9099 $", correct: false },
+          { text: "1290 $", correct: false }
+        ]), //Question component : elements to create the question and the answers
+      }
     };
     this.memorizeArticle = this.memorizeArticle.bind(this)
   };
@@ -33,45 +33,50 @@ class App extends Component {
     this.setState({ answerStatus: answer.correct, answered: true, buttonClicked: buttonIndex, isButtonDisabled: true })
   }
 
-  //change color of clicked button according to correctness of the answer in the button
+  //change color of clicked button according to correctness of the answer and the button clicked
   defineButtonColor = (buttonIndex) => {
-      if (this.state.buttonClicked === buttonIndex
-          && this.state.answered
-          && this.state.answerStatus) {
-          return "success";
-      }
-      else if (this.state.buttonClicked === buttonIndex
-          && this.state.answered
-          && !this.state.answerStatus) {
-          return "danger";
-      }
-      else if (
-          this.state.buttonClicked !== buttonIndex
-          && this.state.answered
-          && !this.state.answerStatus
-          && this.state.question.answers[buttonIndex].correct) {
-          return "success"
-      }
-
-      return ""
+    if (this.state.buttonClicked === buttonIndex
+      && this.state.answered
+      && this.state.answerStatus) {
+      return "success";
+    }
+    else if (this.state.buttonClicked === buttonIndex
+      && this.state.answered
+      && !this.state.answerStatus) {
+      return "danger";
+    }
+    else if (
+      this.state.buttonClicked !== buttonIndex
+      && this.state.answered
+      && !this.state.answerStatus
+      && this.state.question.answers[buttonIndex].correct) {
+      return "success"
+    }
+    else if (
+      this.state.buttonClicked !== buttonIndex
+      && this.state.answered
+      && !this.state.question.answers[buttonIndex].correct) {
+      return "secondary"
+    }
+    return ""
   }
   //to randomize the order of apperance of the answers on screen
   randomizeAnswersDisplay = (array) => {
-      let currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
-      while (0 !== currentIndex) {
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
-      }
-      return array
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array
   }
 
-    /*This method allow us to add elements from API in the array, it check if the array is empty,
-  and create a new one to add objects at the next test (method calls) */
+  /*This method allow us to add elements from API in the array, it check if the array is empty,
+and create a new one to add objects at the next test (method calls) */
 
   memorizeArticle() {
     this.setState(function (prevState) {
@@ -91,7 +96,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Question state={this.state} isCorrectAnswer={this.isCorrectAnswer} defineButtonColor={this.defineButtonColor}/>
+        <Question questionState={this.state} isCorrectAnswer={this.isCorrectAnswer} defineButtonColor={this.defineButtonColor} />
         <ArticleSetChoice currentArticle={this.state} addCurrentArticle={this.memorizeArticle} />
       </div>
     );
