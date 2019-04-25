3fc8c7e745a782c14fc2258006018f5b9928e4c8
Matthieu NICOLAS <mtthncls@gmail.com>
2019-04-25 11:55:34 +0200

    making try again button functionnal in recap

--- src/App.js
+++ src/App.js
@@ -24,22 +24,21 @@ class App extends Component {
       isQuestionAnswered: false, //Question component : has one button been clicked or not ?
       buttonClicked: "",   //Question component : which button has been clicked ?
       isButtonDisabled: false, //Question component : button clickable or not
-      categories : ["Animals", "Sport", "Books", "Films", "Music", "Video Games", 
-                    "Mythology", "Celebrities", "General Knowledge", "Television", "Geography", "History"],
+      categories: ["Animals", "Sport", "Books", "Films", "Music", "Video Games",
+        "Mythology", "Celebrities", "General Knowledge", "Television", "Geography", "History"],
       currentArticleID: 0,
       questionsCategory: 21,
       numberOfQuestions: 10,
       isQuizzLaunched: false, //launch quizz when Play button is clicked and state switched to true
       isNewsDisplayed: false,
-      isThemePageDispolayed : false,
       isAnswerCorrect: false, //Question component : is the answer true or false ?
       isArticleDisplayed: false,
       currentNewsArticle: {}, //ArticleSetChoice component
       preferredNewsArticles: [], //ArticleSetChoice component
-      isArticlesRecapDisplayed : false,
+      isArticlesRecapDisplayed: false,
       correctAnswersCounter: 0
-      };
     };
+  };
 
 
   //to randomize the order of apperance of the answers on screen
@@ -129,7 +128,7 @@ class App extends Component {
 
   //allows to display article selection page after clicking 'next' button
   triggerArticleChoiceDisplay = () => {
-    this.setState({ isQuestionDisplayed: false, isButtonDisabled: false, isArticleDisplayed: true  });
+    this.setState({ isQuestionDisplayed: false, isButtonDisabled: false, isArticleDisplayed: true });
 
     //These three const get the current Date, Month, and Date
     const year = new Date().getFullYear();
@@ -139,8 +138,10 @@ class App extends Component {
     fetch(`https://newsapi.org/v2/everything?q=${this.state.questions[0].category}
            &from=${year}-${month}-${date}&sortBy=publishedAt&apiKey=8ff3d2c7ecb44abaa9d1db3eae9dfcc8`)
       .then(response => response.json())
-      .then(responseInJson => this.setState({ currentNewsArticle: responseInJson.articles, 
-                                              isArticleDisplayed: true }));
+      .then(responseInJson => this.setState({
+        currentNewsArticle: responseInJson.articles,
+        isArticleDisplayed: true
+      }));
   };
 
   /*This method allow us to add elements from API in the array, it check if the array is empty,
@@ -156,77 +157,81 @@ class App extends Component {
     this.setState({
       isQuestionAnswered: false, isQuestionDisplayed: true,
       currentQuestionID: this.state.currentQuestionID + 1,
-      currentArticleID: this.state.currentArticleID + 1, 
-      isArticleDisplayed:false,
+      currentArticleID: this.state.currentArticleID + 1,
+      isArticleDisplayed: false,
     });
   };
   /*go to the next question when click on No button*/
   nextQuestion = () => {
     this.setState({
-      isQuestionAnswered: false, 
-      isQuestionDisplayed: true, 
+      isQuestionAnswered: false,
+      isQuestionDisplayed: true,
       currentQuestionID: this.state.currentQuestionID + 1,
-      currentArticleID: this.state.currentArticleID + 1, 
-      isArticleDisplayed:false,
+      currentArticleID: this.state.currentArticleID + 1,
+      isArticleDisplayed: false,
     });
   };
 
   pickUpCategory = (category) => {
-      switch(category){
-        case "Sport" :
-          this.setState({ questionsCategory : 21 });
-          break;
-        case "Animals" :
-        this.setState({ questionsCategory : 27 });
-          break;
-          case "Books" :
-        this.setState({ questionsCategory : 10 });
-          break;
-          case "Films" :
-        this.setState({ questionsCategory : 11 });
-          break;
-          case "Music" :
-        this.setState({ questionsCategory : 12 });
-          break;
-          case "Video Games" :
-        this.setState({ questionsCategory : 15 });
-          break;
-          case "Mythology" :
-        this.setState({ questionsCategory : 20 });
-          break;
-          case "Celebrities" :
-        this.setState({ questionsCategory : 26 });
-          break;
-          case "Television" :
-        this.setState({ questionsCategory : 14 });
-          break;
-          case "General Knowledge" :
-        this.setState({ questionsCategory : 9 });
-          break;
-          case "Geography" :
-        this.setState({ questionsCategory : 22 });
-          break;
-          case "History" :
-        this.setState({ questionsCategory : 23 });
-          break;
-        default :
-      };
+    switch (category) {
+      case "Sport":
+        this.setState({ questionsCategory: 21 });
+        break;
+      case "Animals":
+        this.setState({ questionsCategory: 27 });
+        break;
+      case "Books":
+        this.setState({ questionsCategory: 10 });
+        break;
+      case "Films":
+        this.setState({ questionsCategory: 11 });
+        break;
+      case "Music":
+        this.setState({ questionsCategory: 12 });
+        break;
+      case "Video Games":
+        this.setState({ questionsCategory: 15 });
+        break;
+      case "Mythology":
+        this.setState({ questionsCategory: 20 });
+        break;
+      case "Celebrities":
+        this.setState({ questionsCategory: 26 });
+        break;
+      case "Television":
+        this.setState({ questionsCategory: 14 });
+        break;
+      case "General Knowledge":
+        this.setState({ questionsCategory: 9 });
+        break;
+      case "Geography":
+        this.setState({ questionsCategory: 22 });
+        break;
+      case "History":
+        this.setState({ questionsCategory: 23 });
+        break;
+      default:
+    };
   };
-  
+
   //This method allow to display "Play" button at the beginning of the quizz, onClick = switch the state fromfalse to true (si isquizzlaunched:true, display question : true, article true 
   chooseUsername = (event) => {
-    this.setState({ isHomePageDisplayed: false, 
-                    isThemePageDisplayed: true});
+    this.setState({
+      isHomePageDisplayed: false,
+      isThemePageDisplayed: true
+    });
     event.preventDefault();
   };
 
   usernameChange = (event) => {
-    this.setState({nameRegistered: event.target.value});
+    this.setState({ nameRegistered: event.target.value });
   };
 
   chooseCategory = () => {
-    this.setState({ isThemePageDisplayed : false, 
-                    isCustomizePageDisplayed : true });
+    this.setState({
+      isThemePageDisplayed: false,
+      isCustomizePageDisplayed: true
+    });
 
     // method for API call
     fetch(`https://opentdb.com/api.php?amount=${this.state.numberOfQuestions}&category=
@@ -257,36 +262,64 @@ class App extends Component {
   };
 
   QuizzCustomize = () => {
-    this.setState({isCustomizePageDisplayed: false, 
-                   isQuestionDisplayed: true})
-  }
+    this.setState({
+      isCustomizePageDisplayed: false,
+      isQuestionDisplayed: true
+    })
+  };
 
-
+  TryAgain = () => {
+    this.setState({
+      buttonClicked: "",
+      categories: ["Animals", "Sport", "Books", "Films", "Music", "Video Games", "Mythology", "Celebrities", "General Knowledge", "Television", "Geography", "History"],
+      correctAnswersCounter: 0,
+      currentArticleID: 0,
+      currentNewsArticle: {},
+      currentQuestionID: 0,
+      isAnswerCorrect: false,
+      isArticleDisplayed: false,
+      isArticlesRecapDisplayed: false,
+      isButtonDisabled: false,
+      isCustomizePageDisplayed: false,
+      isHomePageDisplayed: false,
+      isNewsDisplayed: false,
+      isQuestionAnswered: false,
+      isQuestionDisplayed: false,
+      isQuestionLoading: true,
+      isQuizzLaunched: false,
+      isThemePageDisplayed: true,
+      nameRegistered: this.state.nameRegistered,
+      numberOfQuestions: 10,
+      preferredNewsArticles: [],
+      questions: [],
+      questionsCategory: 21
+    })
+  };
   render() {
     return (
       <div className="App">
-        {this.state.isHomePageDisplayed && 
-        <HomePage chooseUsername={this.chooseUsername} 
-                  usernameChange={this.usernameChange} 
-                  nameRegistered={this.state.nameRegistered} />}
-        {this.state.isThemePageDisplayed && 
-        <Categories chooseCategory={this.chooseCategory} 
-                    pickUpCategory={this.pickUpCategory} 
-                    categories={this.state.categories}/>}
-        {this.state.isCustomizePageDisplayed && 
-        <CustomizeQuizz QuizzCustomize={this.QuizzCustomize}/>}
+        {this.state.isHomePageDisplayed &&
+          <HomePage chooseUsername={this.chooseUsername}
+            usernameChange={this.usernameChange}
+            nameRegistered={this.state.nameRegistered} />}
+        {this.state.isThemePageDisplayed &&
+          <Categories chooseCategory={this.chooseCategory}
+            pickUpCategory={this.pickUpCategory}
+            categories={this.state.categories} />}
+        {this.state.isCustomizePageDisplayed &&
+          <CustomizeQuizz QuizzCustomize={this.QuizzCustomize} />}
         {this.displayLoading()}
         {this.state.isQuestionDisplayed && this.displayQuestions()}
-        {this.state.isButtonDisabled && 
-        <Button onClick={this.triggerArticleChoiceDisplay}>Next</Button>}
-        {!this.state.isQuestionDisplayed && 
+        {this.state.isButtonDisabled &&
+          <Button onClick={this.triggerArticleChoiceDisplay}>Next</Button>}
+        {!this.state.isQuestionDisplayed &&
           this.state.currentNewsArticle.length > 0 &&
-           <ArticleSetChoice currentArticle={this.state.currentNewsArticle[this.state.currentArticleID]} 
-                             addCurrentArticle={this.memorizeArticle} 
-                             nextQuestion={this.nextQuestion}/>}
-        <ArticlesRecap articlesToRecap={this.state.preferredNewsArticles} 
-                       correctAnswersCounter={this.state.correctAnswersCounter}
-                       questions={this.state.questions} />
+          <ArticleSetChoice currentArticle={this.state.currentNewsArticle[this.state.currentArticleID]}
+            addCurrentArticle={this.memorizeArticle}
+            nextQuestion={this.nextQuestion} />}
+        <ArticlesRecap articlesToRecap={this.state.preferredNewsArticles}
+          correctAnswersCounter={this.state.correctAnswersCounter}
+          questions={this.state.questions} tryButton={this.TryAgain} />
       </div>
     );
   };
--- src/Components/ArticlesRecap.js
+++ src/Components/ArticlesRecap.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ArticleSaved from './ArticleSaved';
 import './ArticlesRecap.css'
-const ArticlesRecap = ({ articlesToRecap, correctAnswersCounter, questions }) => {
+const ArticlesRecap = ({ articlesToRecap, correctAnswersCounter, questions, tryButton }) => {
     return (
         <div>
             <h1>Lorem ipsum Username {/* Replace Username by the user input at the beginning*/}</h1>
             <p className="answersCounter"> You have <span className="correctAnswers">{correctAnswersCounter}</span> correct answers on <span className="correctAnswers">{questions.length}</span> questions</p>
 
             {/* offer something to show if user don't keep any articles*/}
-            {articlesToRecap.length === 0 
-            ? <h1>No articles saved</h1>
-            : articlesToRecap.map((article, index) =>
-                <ArticleSaved key={index} articlesToRecap={article} />)};
+            {articlesToRecap.length === 0
+                ? <h1>No articles saved</h1>
+                : articlesToRecap.map((article, index) =>
+                    <ArticleSaved key={index} articlesToRecap={article} />)}
 
             {/* that button must send back to the 1st page of the app */}
-            <button>Try again</button>
+            <button onClick={tryButton}>Try again</button >
         </div>
     );
 }
