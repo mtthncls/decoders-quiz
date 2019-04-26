d9876130d5dc899a243117fa95b0c5314b29277f
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-26 11:12:36 +0200

    The style has been done

--- src/Components/ArticleSaved.js
+++ src/Components/ArticleSaved.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import { Card, CardText, Container, Row, Col} from 'reactstrap';
+import { Card, CardText, Row, Col} from 'reactstrap';
 
 const ArticleSaved = ({articlesToRecap}) => {
     return ( 
-        <article>
-            <Container>
-                <Card className="mt-3" body inverse color="info">
-                    <Row>
-                        <Col sm="12" lg="6">
+        <div className="cardArticle">
+            <Card className="mt-3 " body inverse color="info">
+                <Row>
+                    <Col sm="12" lg="6">
+                        <CardText>
+                            <img width="50%" src={articlesToRecap.urlToImage} alt="Article"></img>
+                        </CardText>
+                    </Col>
+                    <Col sm="12" lg="6">
+                        <h3>
                             <CardText>
-                                <img width="50%" src={articlesToRecap.urlToImage} alt="Article"></img>
+                                <a href={articlesToRecap.url}><span className="articletitle">{articlesToRecap.title}</span></a>
                             </CardText>
-                        </Col>
-                        <Col sm="12" lg="6">
-                            <h3>
-                                <CardText>
-                                    <a href={articlesToRecap.url}>{articlesToRecap.title}</a>
-                                </CardText>
-                            </h3>
-                        </Col>
-                    </Row>
-                </Card>
-            </Container>
-        </article>
+                        </h3>
+                    </Col>
+                </Row>
+            </Card>
+        </div>
      );
 }
  
--- src/Components/ArticlesRecap.js
+++ src/Components/ArticlesRecap.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ArticleSaved from './ArticleSaved';
+import { Container } from 'reactstrap';
 import './ArticlesRecap.css'
+
 const ArticlesRecap = ({ articlesToRecap, correctAnswersCounter, questions, tryButton }) => {
     return (
         <div>
-            <h1>Lorem ipsum Username {/* Replace Username by the user input at the beginning*/}</h1>
-            <p className="answersCounter"> You have <span className="correctAnswers">{correctAnswersCounter}</span> correct answers on <span className="correctAnswers">{questions.length}</span> questions</p>
+            <Container>
+                <h1>Lorem ipsum Username {/* Replace Username by the user input at the beginning*/}</h1>
+                <p className="answersCounter"> You have <span className="correctAnswers">{correctAnswersCounter}</span> correct answers on <span className="correctAnswers">{questions.length}</span> questions</p>
 
-            {/* offer something to show if user don't keep any articles*/}
-            {articlesToRecap.length === 0 
-            ? <h1>No articles saved</h1>
-            : articlesToRecap.map((article, index) =>
-                <ArticleSaved key={index} articlesToRecap={article} />)}
+                {/* offer something to show if user don't keep any articles*/}
+                {articlesToRecap.length === 0 
+                ? <h1>No articles saved</h1>
+                : articlesToRecap.map((article, index) =>
+                    <ArticleSaved key={index} articlesToRecap={article} />)}
 
-            {/* that button sends back to the "choose category" page of the app */}
-            <button onClick={tryButton}>Try again</button >
+                {/* that button sends back to the "choose category" page of the app */}
+                <button className={"tryAgain"} onClick={tryButton}><span className="Tryagaintext">Try again</span></button >
+            </Container>
         </div>
     );
 }
