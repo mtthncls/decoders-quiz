e91370984f0c365c2beff189cb9d6be80a428bb2
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-04-29 15:56:21 +0200

    put car-support style on first page

--- src/Categories.js
+++ src/Categories.js
@@ -5,10 +5,12 @@ import './App.css';
 
 const Categories = ({categories, pickUpCategory,categoryChoice, chooseCategory}) => {
     return ( 
-        <Container className="card-support">
-            <header className="title">
-                <h1>Step 1/2 : Please choose category...</h1>
+        <Container>
+            <header className="card-support">
+                <h1>Step 1/2 : Category</h1>
             </header>
+            <div className="card-support">
+            <h3>First, you have to choose your category...</h3>
             <Row>
                 {categories.map((category, index) => 
                 <CategoryChoice pickUpCategory={() => pickUpCategory(category)} 
@@ -16,6 +18,7 @@ const Categories = ({categories, pickUpCategory,categoryChoice, chooseCategory})
             </Row>
             <div className="zoom-button transition">
             <Button onClick={chooseCategory}>Next</Button>
+            </div>
             </div>
         </Container>
     );
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,21 +6,24 @@ import '../App.css';
 const HomePage = ({chooseUsername, usernameChange, nameRegistered}) => {
     return(        
         <Container>
-            <header>
-                <h1>Décodeurs Quizz</h1>
+            <header className="card-support head">
+                <h1>Decoder's Quizz</h1>
             </header>
+            <div className="card-support">
+            <h3>Welcome to the quiz, please choose your username :</h3>
             <form>
                 <label htmlFor="Username">
-                    <input placeholder="Username" name="Username" id="Username" 
+                    <input className="input-container" placeholder="Username" name="Username" id="Username" 
                            onChange={usernameChange} value={nameRegistered}/>
                 </label>
             </form>
              <br></br>
-            <Row className="zoom-button">
+            <Row className="zoom-button transition">
                 <Col sm="12" md={{ size: 6, offset: 3 }}>
                     <Button type="submit" name="submit" color="success" onClick={chooseUsername}>PLAY</Button>
                 </Col>
             </Row>
+            </div>
         </Container>             
     )
 }
