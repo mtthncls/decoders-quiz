f71175b5884faba2ea5c654e329f38b295cc485e
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-29 17:24:17 +0200

    Changing

--- src/Categories.js
+++ src/Categories.js
@@ -3,7 +3,7 @@ import { Button, Container, Row } from 'reactstrap';
 import CategoryChoice from './Components/CategoryChoice';
 import './App.css';
 
-const Categories = ({categories, pickUpCategory,categoryChoice, chooseCategory}) => {
+const Categories = ({categories, pickUpCategory, categoryChoice, chooseCategory, test}) => {
     return ( 
         <Container>
             <header className="card-support">
@@ -13,8 +13,9 @@ const Categories = ({categories, pickUpCategory,categoryChoice, chooseCategory})
             <h3>First, you have to choose your category...</h3>
             <Row>
                 {categories.map((category, index) => 
-                <CategoryChoice pickUpCategory={() => pickUpCategory(category)} 
-                                category={category} key={index} categoryChoice={categoryChoice}/>)}
+                <CategoryChoice pickUpCategory={() => pickUpCategory(category, index)} 
+                                category={category} key={index} categoryChoice={categoryChoice}
+                                specificButton={index} test={test}/>)}
             </Row>
             <div className="zoom-button transition">
             <Button onClick={chooseCategory}>Next</Button>
