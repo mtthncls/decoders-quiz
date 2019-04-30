ef26a1aea53aace065c590d370dd310cdd202e1f
Sébastien Jolly <seb_879@hotmail.fr>
2019-04-30 15:44:55 +0200

    correcting typo

--- src/Components/CategoryChoice.js
+++ src/Components/CategoryChoice.js
@@ -6,7 +6,7 @@ const CategoryChoice = ({ category, pickUpCategory, catTitle }) => {
     return (
         <Col md={6} lg={2}>
             <div className="zoom-button category">
-                <Button className={catTitle === category ? "TOTO" : ""} onClick={pickUpCategory}>{category}</Button>
+                <Button className={catTitle === category ? "category-chosen" : ""} onClick={pickUpCategory}>{category}</Button>
             </div>
         </Col>
     )
