b714f65c26ab9c38ba46885d5af2a01c814df6f4
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-30 14:42:13 +0200

    Styling

--- src/Components/ArticlesRecap.js
+++ src/Components/ArticlesRecap.js
@@ -12,7 +12,7 @@ const ArticlesRecap = ({ articlesToRecap, correctAnswersCounter, questions, tryB
                 <p className="answersCounter"> You have <span className="correctAnswers">{correctAnswersCounter}</span> correct answers on <span className="correctAnswers">{questions.length}</span> questions</p>
             </Container>
             <Container className="card-support mt-5">
-                <h2>Here's your reading list : </h2>
+                <h1>Here's your reading list : </h1>
             </Container>
             {/* offer something to show if user don't keep any articles*/}
             {articlesToRecap.length === 0 
